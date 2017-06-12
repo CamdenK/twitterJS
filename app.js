58b0541e7a2d002008d3ddb517bd7c748cbbcead
Camden Ko <camdenko@gmail.com>
2017-06-12 17:05:07 -0500

    fixed npm start

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.use(function(request, response, next){
+  console.log(request.method, request.path)
+  next()
+})
 app.get('/', function(request, response){
   response.send('Welcome to Bitter')
+})
+
+app.get('/news', function(request, response){
+  response.send('News')
 })
 
 app.listen(3000, function(){
