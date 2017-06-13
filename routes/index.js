6f57ce4f792ebad6d2fa8970ba7c82b862224135
Madelyn Romzek <mrromzek@umich.edu>
2017-06-13 13:42:08 -0500

    creating form submission route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 router.use(express.static('public'));
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({extended: false})
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
@@ -12,8 +15,14 @@ router.get('/', function (req, res) {
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   var list = tweetBank.find( {name: name} );
-  res.render( 'index', { tweets: list, showForm: true});
+  res.render( 'index', { tweets: list, showForm: false});
 });
 
+router.post('/tweets', bodyParser, function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+});
 
 module.exports = router;
