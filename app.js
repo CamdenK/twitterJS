3b6c917407bdf5ffbf8977bfe79f3d66929cf3ff
Madelyn Romzek <mrromzek@umich.edu>
2017-06-13 15:00:23 -0500

    added morgan and removed an app.use

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const tweetBank = require('./tweetBank.js')
 const app = express()
 const routes = require('./routes')
 
+const logger = morgan('dev')
+app.use(logger);
+
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({extended: false})
@@ -15,14 +18,8 @@ var socketio = require('socket.io')
 var server = app.listen(3000)
 var io = socketio.listen(server)
 
-
 app.set('view engine', 'html')
 app.engine('html', nunjucks.render)
 nunjucks.configure('views', {noCache: true})
-
-app.use(function(request, response, next){
-  console.log(request.method, request.path)
-  next()
-})
 
 app.use('/', routes(io))
